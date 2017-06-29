ecf6cc1b306181a5d4c4c483686c2bd82e832e3c
Thibaut Brandscheid <randaltor@web.de>
2017-06-29 12:16:27 +0200

    split controler

--- src/Controler.hs
+++ src/Controler.hs
@@ -2,9 +2,7 @@ module Controler(run) where
 
 import Config
 import View
-import Control.Concurrent(threadDelay)
-
-type Seconds = Int
+import Controler.Internal
 
 
 run :: Config -> IO ()
@@ -15,15 +13,3 @@ run cfg =
         waitAndNotify (get timeValue) (get timeType)
         if get isPlaySound then playSound $ get soundPath else mempty
     where get f = f cfg
-
-waitAndNotify :: Seconds -> TimeType -> IO ()
-waitAndNotify n tt = do
-    threadDelay $ 1000000 * n
-    let p   = if tt == Second then n else n `div` 60
-        msg = show p ++ " " ++ timeTypeToName tt n ++ " passed"
-    showMessage msg >> showNotification msg
-
-timeTypeToName :: TimeType -> Seconds -> String
-timeTypeToName tt n = case tt of
-    Minute -> if n == 60 then "minute" else "minutes"
-    Second -> if n == 1  then "second" else "seconds"
---
+++ src/Controler/Internal.hs
@@ -0,0 +1,22 @@
+module Controler.Internal
+    ( waitAndNotify
+    , timeTypeToName
+    ) where
+
+import Control.Concurrent(threadDelay)
+import Config(TimeType(..))
+import View
+
+type Seconds = Int
+
+waitAndNotify :: Seconds -> TimeType -> IO ()
+waitAndNotify n tt = do
+    threadDelay $ 1000000 * n
+    let p   = if tt == Second then n else n `div` 60
+        msg = show p ++ " " ++ timeTypeToName tt n ++ " passed"
+    showMessage msg >> showNotification msg
+
+timeTypeToName :: TimeType -> Seconds -> String
+timeTypeToName tt n = case tt of
+    Minute -> if n == 60 then "minute" else "minutes"
+    Second -> if n == 1  then "second" else "seconds"
